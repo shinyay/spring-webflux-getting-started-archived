a9b921f66a298e07feb71cd1dbe22f9f7234591c
shinyay <shinya.com@gmail.com>
2020-10-29 23:23:02 +0900

    Add Mono function

--- src/main/kotlin/handler/HelloHandler.kt
+++ src/main/kotlin/handler/HelloHandler.kt
@@ -1,7 +1,23 @@
 package handler
 
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Component
 class HelloHandler {
+
+    fun hello(request: ServerRequest): Mono<ServerResponse> {
+        val json = """
+                |{
+                |    "message" : "Hello"
+                |}
+        """.trimIndent()
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Flux.just(json), String::class.java)
+    }
 }
