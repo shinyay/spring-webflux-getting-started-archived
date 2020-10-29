8378a2543d5f33c396e065deccb2626c25cec2b5
shinyay <shinya.com@gmail.com>
2020-10-29 22:33:30 +0900

    Initial Commit

---
+++ src/main/kotlin/com/google/shinyay/springwebfluxgettingstarted/SpringWebfluxGettingStartedApplication.kt
@@ -0,0 +1,11 @@
+package com.google.shinyay.springwebfluxgettingstarted
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxGettingStartedApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebfluxGettingStartedApplication>(*args)
+}
