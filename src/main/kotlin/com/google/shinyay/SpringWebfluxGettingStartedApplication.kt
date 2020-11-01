7e0ce739790d298c7bfd8f6273071aaf0ca4ede0
shinyay <shinya.com@gmail.com>
2020-11-01 23:12:28 +0900

    Add WebApplicationType.REACTIVE

--- src/main/kotlin/com/google/shinyay/SpringWebfluxGettingStartedApplication.kt
+++ src/main/kotlin/com/google/shinyay/SpringWebfluxGettingStartedApplication.kt
@@ -2,14 +2,20 @@ package com.google.shinyay
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SpringWebfluxGettingStartedApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringWebfluxGettingStartedApplication>(*args)
+//	runApplication<SpringWebfluxGettingStartedApplication>(*args)
+	SpringApplicationBuilder()
+			.sources(SpringWebfluxGettingStartedApplication::class.java)
+			.web(WebApplicationType.REACTIVE)
+			.run(*args)
 }
 
 val Any.logger: Logger
