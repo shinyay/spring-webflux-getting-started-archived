ba00d9bdfa7b1962f9c8ded10eaaa6222c6f690d
shinyay <shinya.com@gmail.com>
2020-11-08 22:58:19 +0900

    Add call webclient

--- src/main/kotlin/com/google/shinyay/client/HelloClient.kt
+++ src/main/kotlin/com/google/shinyay/client/HelloClient.kt
@@ -1,7 +1,12 @@
 package com.google.shinyay.client
 
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 
-class HelloClient(val client: WebClient = WebClient.create("http://localhost:8080")) {
 
+class HelloClient(val client: WebClient = WebClient.create("http://localhost:8080")) {
+    var result = client.get()
+            .uri("/hello")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
 }
