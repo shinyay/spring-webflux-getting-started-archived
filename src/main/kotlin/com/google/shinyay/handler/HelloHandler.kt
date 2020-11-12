b7514b12f7b8de41d4e820ccf9234c5efaaf3d19
shinyay <shinya.com@gmail.com>
2020-11-12 23:47:40 +0900

    Add current time to show for the result

--- src/main/kotlin/com/google/shinyay/handler/HelloHandler.kt
+++ src/main/kotlin/com/google/shinyay/handler/HelloHandler.kt
@@ -6,14 +6,18 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Component
 class HelloHandler {
 
     fun hello(request: ServerRequest): Mono<ServerResponse> {
+        val time = ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
         val json = """
                 |{
-                |    "message" : "Hello"
+                |    "message" : "Hello at $time"
                 |}
         """.trimMargin()
         return ServerResponse.ok()
