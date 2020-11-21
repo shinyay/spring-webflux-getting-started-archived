d1c0d50948b825027b1a48cc602f9455f8f3a492
shinyay <shinya.com@gmail.com>
2020-11-21 22:02:00 +0900

    Add WebTestClient for Non-blocking, reactive client for testing web servers

--- src/test/kotlin/com/google/shinyay/HelloRouterTest.kt
+++ src/test/kotlin/com/google/shinyay/HelloRouterTest.kt
@@ -2,9 +2,10 @@ package com.google.shinyay
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HelloRouterTest {
+class HelloRouterTest(val webTestClient: WebTestClient) {
 
 	@Test
 	fun contextLoads() {
