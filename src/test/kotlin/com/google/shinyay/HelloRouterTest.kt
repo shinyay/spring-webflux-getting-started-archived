40e1803d1874989b727f52e5db3ca0801f8de30d
shinyay <shinya.com@gmail.com>
2020-11-22 22:04:19 +0900

    Add hello controller test

--- src/test/kotlin/com/google/shinyay/HelloRouterTest.kt
+++ src/test/kotlin/com/google/shinyay/HelloRouterTest.kt
@@ -2,13 +2,19 @@ package com.google.shinyay
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HelloRouterTest(val webTestClient: WebTestClient) {
 
 	@Test
-	fun contextLoads() {
+	fun testHello() {
+		webTestClient
+				.get().uri("/hello")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk
 	}
 
 }
